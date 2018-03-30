781a3218ef67f05bc58c06cf94c54c46f5f397e5
Terese Haimberger <haimberger@fitanalytics.com>
2018-03-30 22:22:56 +0200

    added ExactEqualer that compares values exactly

---
+++ basic/exact.go
@@ -0,0 +1,24 @@
+package basic
+
+// ExactEqualer compares values exactly.
+type ExactEqualer struct{}
+
+// Bool compares two Boolean values exactly.
+func (tc ExactEqualer) Bool(a, b bool) bool {
+	return a == b
+}
+
+// Int compares two integer values exactly.
+func (tc ExactEqualer) Int(a, b int) bool {
+	return a == b
+}
+
+// Float64 compares two floating-point numbers exactly.
+func (tc ExactEqualer) Float64(a, b float64) bool {
+	return a == b
+}
+
+// String compares two string values exactly.
+func (tc ExactEqualer) String(a, b string) bool {
+	return a == b
+}
---
+++ basic/exact_test.go
@@ -0,0 +1,21 @@
+package basic
+
+import (
+	"testing"
+)
+
+func TestBool_Exact(t *testing.T) {
+	TestExactBool(t, ExactEqualer{})
+}
+
+func TestInt_Exact(t *testing.T) {
+	TestExactInt(t, ExactEqualer{})
+}
+
+func TestFloat64_Exact(t *testing.T) {
+	TestExactFloat64(t, ExactEqualer{})
+}
+
+func TestString_Exact(t *testing.T) {
+	TestExactString(t, ExactEqualer{})
+}
